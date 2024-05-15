ee016943f19f0362d8684df037ff8e4042451a2c
Andrew Halisky <ajhalisky@gmail.com>
2024-05-15 17:34:48 -0400

    Made additional final changes to the project

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,6 +5,6 @@ const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
-router.get("/type/detail/:itemId", invController.buildClassificationDetails);
+router.get("/detail/:itemId", invController.buildClassificationDetails);
 
 module.exports = router;
