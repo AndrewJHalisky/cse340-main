f10e1fb110ad0fa5bd852280e85b5578c2ca0602
Andrew Halisky <ajhalisky@gmail.com>
2024-05-16 13:55:41 -0400

    Changed require statement

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get("/", function(req, res){
 // Inventory routes
 app.use("/inv", inventoryRoute)
 // Account routes
-app.use("/account", require("./routes/accountRoute"))
+app.use("/account", require("../routes/accountRoute"))
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
