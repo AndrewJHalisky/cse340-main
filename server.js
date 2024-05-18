a5c4bd8949ffc9ffb2d3a4956f465e085c12ec42
Andrew Halisky <ajhalisky@gmail.com>
2024-05-18 16:42:00 -0400

    Changed the sequence

--- server.js
+++ server.js
@@ -32,8 +32,6 @@ app.use(session({
   saveUninitialized: true,
   name: 'sessionId',
 }))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
@@ -41,7 +39,8 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 /* ***********************
  * View Engine and Templates
  *************************/
